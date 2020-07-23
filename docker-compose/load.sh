4c3ec35f9bc7191044db72de158f69d127cabeda
Wayne Brown <wayne.brown@appdynamics.com>
2020-07-22 20:17:21 -0600

    Update load.sh

--- docker-compose/load.sh
+++ docker-compose/load.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
-dockerize -wait tcp://core-services:8080 -wait tcp://mongo-accounts:27017 -wait tcp://mongo-sessions:27017 -timeout 300s
-
-urls=("http://web-api:3000/api/login" "http://web-api:3000/api/about" "http://web-api:3000/api/search" "http://web-api:3000/api/logout")
-
 while true
 do
-    for url in ${urls[@]}; do
-        echo $(curl -s $url)
+    url_start=("http://localhost:3000/api/login" "http://web-api:3000/api/account")
+
+    job_seeker_urls=("http://localhost:3000/api/search" "http://localhost:3000/api/submitCandidate")
+    job_poster_urls="http://localhost:3000/api/addJob"
+
+    urls=("${url_start[@]}")
+
+    # Randomly toggle between a job searcher and a job poster
+    rnd=$(((RANDOM % 3)+ 1))   
+    i=1  
+    if [ $((rnd)) -gt 1 ]; then
+        num_times=$(((RANDOM % 5)+ 1))        
+        while [ $i -lt $num_times ]
+        do
+            urls+=("${job_seeker_urls[@]}")
+            i=$((i+1))
+        done        
+    else
+        num_times2=$(((RANDOM % 5)+ 1))        
+        while [ $i -lt $num_times2 ]
+        do
+            urls+=("$job_poster_urls")
+            i=$((i+1))
+        done
+    fi
+
+    urls+=("http://localhost:3000/api/logout")
+
+    for url in ${urls[@]}
+    do        
+        echo "curl ${url}"
         sleep 1
     done    
 done
